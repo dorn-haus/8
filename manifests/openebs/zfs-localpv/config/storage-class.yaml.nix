adfd0314bcf88cf18f476e5ca1793dd28850f19d
Attila Oláh <attila@dorn.haus>
2024-11-08 23:28:47 +0100

    Add ZFS storage class.

---
+++ manifests/openebs/zfs-localpv/config/storage-class.yaml.nix
@@ -0,0 +1,13 @@
+{
+  kind = "StorageClass";
+  apiVersion = "storage.k8s.io/v1";
+  metadata.name = "openebs-zfspv";
+  parameters = {
+    recordsize = "128k";
+    compression = "off";
+    dedup = "off";
+    fstype = "zfs";
+    poolname = "zfspv";
+  };
+  provisioner = "zfs.csi.openebs.io";
+}
--- manifests/openebs/zfs-localpv/ks.yaml.nix
+++ manifests/openebs/zfs-localpv/ks.yaml.nix
@@ -1,22 +1,32 @@
 let
   name = "zfs-localpv";
   namespace = "openebs";
-in {
-  kind = "Kustomization";
-  apiVersion = "kustomize.toolkit.fluxcd.io/v1";
-  metadata = {
-    inherit name;
-    namespace = "flux-system";
+  path = "./${namespace}/${name}";
+
+  ks = name: spec: {
+    kind = "Kustomization";
+    apiVersion = "kustomize.toolkit.fluxcd.io/v1";
+    metadata = {
+      inherit name;
+      namespace = "flux-system";
+    };
+    spec =
+      {
+        targetNamespace = namespace;
+        commonMetadata.labels."app.kubernetes.io/name" = name;
+        prune = true;
+        sourceRef = import ../../flux-system/source.nix;
+        wait = true;
+        interval = "30m";
+        retryInterval = "1m";
+        timeout = "5m";
+      }
+      // spec;
   };
-  spec = {
-    targetNamespace = namespace;
-    commonMetadata.labels."app.kubernetes.io/name" = name;
-    path = "./${namespace}/${name}/app";
-    prune = true;
-    sourceRef = import ../../flux-system/source.nix;
-    wait = true;
-    interval = "30m";
-    retryInterval = "1m";
-    timeout = "5m";
+
+  app = ks name {path = "${path}/app";};
+  config = ks "${name}-config" {
+    path = "${path}/config";
+    dependsOn = [app.metadata];
   };
-}
+in [app config]
