5977bc4c5107c541d5babfb7fe545d3f62212986
Attila Oláh <attila@dorn.haus>
2024-11-09 20:15:06 +0100

    Basic Grafana ingress config.

--- manifests/monitoring/grafana/app/values.yaml.nix
+++ manifests/monitoring/grafana/app/values.yaml.nix
@@ -1,4 +1,28 @@
-{
-  service.type = "LoadBalancer";
-  # TODO: Configure the ingress instead.
+{self, ...}: {
+  # Expose Grafana via an ingress path on the default hostname.
+  ingress = {
+    enabled = true;
+    path = "/grafana";
+    hosts = [self.lib.cluster.domain];
+
+    # TODO: Set the default ingress class!
+    ingressClassName = "nginx";
+
+    # TODO: Enable SSL redirect:
+    # extraPaths:
+    # - path: /*
+    #   pathType: Prefix
+    #   backend:
+    #     service:
+    #       name: ssl-redirect
+    #       port:
+    #         name: use-annotation
+  };
+
+  # Allow spinning up a second replica. The default number of replicas is 1.
+  autoscaling = {
+    enabled = true;
+    maxReplicas = 2;
+  };
+  podDisruptionBudget.minAvailable = 1;
 }
