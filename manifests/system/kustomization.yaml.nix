a4b4f25a4f50483704448cb5837330269ff1e945
Attila Oláh <attila@dorn.haus>
2024-11-19 22:36:03 +0100

    Add the local-path-provisioner storage class.
    Make it the default storage class since those paths would be on NVMEs,
    and have the ZFS storage class handle slower, long-term storage.

--- manifests/system/kustomization.yaml.nix
+++ manifests/system/kustomization.yaml.nix
@@ -3,6 +3,7 @@
   apiVersion = "kustomize.config.k8s.io/v1beta1";
   resources = [
     "./namespace.yaml"
+    "./local-path-provisioner/ks.yaml"
     "./spegel/ks.yaml"
   ];
 }
---
+++ manifests/system/local-path-provisioner/app/kustomization.yaml.nix
@@ -0,0 +1,14 @@
+let
+  name = "local-path-provisioner";
+in {
+  kind = "Kustomization";
+  apiVersion = "kustomize.config.k8s.io/v1beta1";
+  resources = ["./helm-release.yaml"];
+  configMapGenerator = [
+    {
+      name = "${name}-values";
+      files = ["./values.yaml"];
+    }
+  ];
+  configurations = ["./kustomizeconfig.yaml"];
+}
