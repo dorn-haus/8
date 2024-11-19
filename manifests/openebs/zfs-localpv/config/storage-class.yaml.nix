b32b692885e57d6ea7401d04ed64fdd5d7b61e90
Attila Oláh <attila@dorn.haus>
2024-11-19 22:53:01 +0100

    ZFS localpv tweaks (#32)
    * Set OpenEBS ZFS storage class reclaim policy to retain.
    
    ZFS PVs are for long term storage, let's not remove datasets by accident.
    
    * Enable ZFS encryption.

--- manifests/openebs/zfs-localpv/config/storage-class.yaml.nix
+++ manifests/openebs/zfs-localpv/config/storage-class.yaml.nix
@@ -4,10 +4,12 @@
   metadata.name = "openebs-zfspv";
   parameters = {
     recordsize = "128k";
+    encryption = "on";
     compression = "off";
     dedup = "off";
     fstype = "zfs";
     poolname = "zfspv";
   };
   provisioner = "zfs.csi.openebs.io";
+  reclaimPolicy = "Retain";
 }
