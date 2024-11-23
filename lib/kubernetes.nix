30146fff3b852ecfd0c9a02cf51fd5a1364c7b23
Attila Oláh <attila@dorn.haus>
2024-11-23 17:46:57 +0100

    Remove dummy let-in bindings.

--- lib/kubernetes.nix
+++ lib/kubernetes.nix
@@ -3,17 +3,16 @@
   inherit (lib.attrsets) recursiveUpdate;
   inherit (lib.strings) hasSuffix removeSuffix;
 in {
-  namespace = dir: overrides: let
-    defaults = {
+  namespace = dir: overrides:
+    recursiveUpdate {
       kind = "Namespace";
       apiVersion = "v1";
       metadata.name = baseNameOf dir;
-    };
-  in
-    recursiveUpdate defaults overrides;
+    }
+    overrides;
 
-  kustomization = dir: overrides: let
-    defaults = {
+  kustomization = dir: overrides:
+    recursiveUpdate {
       kind = "Kustomization";
       apiVersion = "kustomize.config.k8s.io/v1beta1";
       resources = filter (item: item != null) (attrValues (mapAttrs (
@@ -24,7 +23,6 @@ in {
           then removeSuffix ".nix" name # non-flux manifest
           else null
       ) (readDir dir)));
-    };
-  in
-    recursiveUpdate defaults overrides;
+    }
+    overrides;
 }
