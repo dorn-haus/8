a4e7fd26482f454de6cb598537b26ef9b883f933
Attila Oláh <attila@dorn.haus>
2024-11-29 22:33:36 +0100

    Use a more strict CIDR for node IP validation.

--- manifests/kube-system/kubelet-csr-approver/app/values.yaml.nix
+++ manifests/kube-system/kubelet-csr-approver/app/values.yaml.nix
@@ -2,6 +2,6 @@
   inherit (self.lib) cluster;
 in {
   providerRegex = "^${cluster.name}-\\d{2}$";
-  providerIpPrefixes = with cluster.network.node; "${cidr4},${cidr6}";
+  providerIpPrefixes = with cluster.network.node; "${cidr4Strict},${cidr6}";
   bypassDnsResolution = true;
 }
