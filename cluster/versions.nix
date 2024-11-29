8c9b240154cd19e29a39d0d5c9b2ca676e769679
Attila Oláh <attila@dorn.haus>
2024-11-29 22:03:27 +0100

    Install descheduler using the default config.
    This uses the default policies as far as I'm aware, wuth a cron job that runs every 30m.

--- cluster/versions.nix
+++ cluster/versions.nix
@@ -6,6 +6,7 @@ in {
   # If transform is not provided, the default is used: (v: v).
   cert-manager.helm = ["https://charts.jetstack.io" "1.16.2"];
   cilium.helm = ["https://helm.cilium.io" "1.16.4"];
+  descheduler.helm = ["https://kubernetes-sigs.github.io/descheduler" "0.31.0"];
   external-secrets.helm = ["https://charts.external-secrets.io" "0.10.7"];
   flux-operator.helm = ["oci://ghcr.io/controlplaneio-fluxcd/charts" "0.10.0"];
   flux.github-releases = ["https://github.com/fluxcd/flux2" "2.4.0"];
