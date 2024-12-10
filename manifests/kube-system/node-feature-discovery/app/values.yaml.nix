5691bb995a5c59cba9bd652a01a5e56005030901
Attila Oláh <attila@dorn.haus>
2024-12-10 22:50:28 +0100

    Remove 2nd NFD master replica.

--- manifests/kube-system/node-feature-discovery/app/values.yaml.nix
+++ manifests/kube-system/node-feature-discovery/app/values.yaml.nix
@@ -1,5 +1,17 @@
 {
-  master.replicaCount = 2;
+  master = {
+    # TODO: Add a second replica.
+    # The master deployment has pod affinity that assigns it to control plane nodes.
+    # Since we currently run with a single contrel plane nodes, that means they both end up on the same node.
+    replicaCount = 1;
+
+    resources.limits = {
+      cpu = "400m";
+      memory = "1Gi";
+    };
+
+    revisionHistoryLimit = 2;
+  };
 
   # Lower resource limits for workers.
   worker.resources.limits = {
