5068974abcc7d35ae249e8dd096fcdc104aedae2
Attila Oláh <attila@dorn.haus>
2025-01-12 17:01:47 +0100

    Do not run Spegel on Alpine nodes.

--- manifests/kube-system/spegel/app/values.yaml.nix
+++ manifests/kube-system/spegel/app/values.yaml.nix
@@ -1,3 +1,15 @@
 {
+  affinity.nodeAffinity.requiredDuringSchedulingIgnoredDuringExecution.nodeSelectorTerms = [
+    {
+      matchExpressions = [
+        {
+          key = "distro";
+          operator = "NotIn";
+          values = ["alpine"];
+        }
+      ];
+    }
+  ];
   spegel.containerdRegistryConfigPath = "/etc/cri/conf.d/hosts";
+  revisionHistoryLimit = 4;
 }
